d0d065ae3014f94100af2edb8a842d7511f580c0
Rick <ricks.homelab@gmail.com>
2020-04-08 10:17:56 +0800

    add derivation from yiq colorspace, but broken

--- color_ugen.py
+++ color_ugen.py
@@ -37,19 +37,69 @@ class ColorUgen:
         self.colors = {}
         self.counter = 0
 
-    def add_hsv(self, hsv_val, quiet=True):
-        rgb_tup = colorsys.hsv_to_rgb(*hsv_val)
+    def add_rgb(self, add_val, color_space='hsv', quiet=True):
+        if color_space == 'yiq':
+            rgb_tup = colorsys.yiq_to_rgb(*add_val)
+        else:
+            rgb_tup = colorsys.hsv_to_rgb(*add_val)
         if rgb_tup[0] < 0 or rgb_tup[1] < 0 or rgb_tup[2] < 0:
             print('RGB error: {}'.format(rgb_tup))
             return
         hex_rgb = '#{:02x}{:02x}{:02x}'.format(int(rgb_tup[0]*255), int(rgb_tup[1]*255), int(rgb_tup[2]*255))
         if not quiet:
-            print('rgb: {}, hsv: ({:.2f}, {:.2f}, {:.2f})'.format(hex_rgb, *hsv_val))
-        self.colors.update({hex_rgb: hsv_val})
+            print('rgb: {}, hsv: ({:.2f}, {:.2f}, {:.2f})'.format(hex_rgb, *add_val))
+        self.colors.update({hex_rgb: add_val})
         self.counter += 1
         return
 
-    def color_gen_list(self, num_cols, debug=False):
+    def color_gen_list(self, num_cols, color_space='hsv', debug=False):
+        if color_space == 'yiq':
+            #return self.color_gen_list_from_yiq(num_cols, debug)[:num_cols]
+            return self.color_gen_list_from_yiq(num_cols, debug)
+        else:
+            #return self.color_gen_list_from_hsv(num_cols, debug)[:num_cols]
+            return self.color_gen_list_from_hsv(num_cols, debug)
+
+    def color_gen_list_from_yiq(self, num_cols, debug=False):
+
+        # Calculate optimal number of steps for num_cols distinct colors
+        num_iqx = 7
+        num_iqy = 7
+        num_y = int(num_cols / (num_iqx * num_iqy))
+        if debug: print('loops: y={}, iqx={}, iqy={}'.format(num_y, num_iqx, num_iqy))
+
+        # Calculate range parameters, start, stop, index.  Subtract from hue stop, to allow drifting start
+        y_params = (10, 90, num_y)  # min, max, num_steps
+        iqx_params = (-100, 100, num_iqx)
+        iqy_params = (-100, 100, num_iqy)
+        if debug: print('params: y={}, iqx={}, iqy={}'.format(y_params, iqx_params, iqy_params))
+
+        # Set parameters for jittering iqx and iqy values between y steps.
+
+        # Set step sizes, always round up
+        y_step = int(((y_params[1] - y_params[0]) / y_params[2]) // 1) + 1
+        iqx_step = int(((iqx_params[1] - iqx_params[0]) / iqx_params[2]) // 1) + 1
+        iqy_step = int(((iqy_params[1] - iqy_params[0]) / iqy_params[2]) // 1) + 1
+        if debug: print('y_step: {}, iqx_step: {}, iqy_step: {}'.format(y_step, iqx_step, iqy_step))
+
+        # This doesn't work, as the color cube is rotated -33deg about the y axis and scaled.
+        # https://hbfs.wordpress.com/2018/05/08/yuv-and-yiq-colorspaces-v/
+        # Step from brightest to darkest
+        for t_y in range(y_params[1], y_params[0], -y_step):
+            yiq_y = t_y / 100
+
+            # Step from lowest to highest iqx
+            for t_iqx in range(iqx_params[0], iqx_params[1], iqx_step):
+                yiq_iqx = t_iqx / 100
+
+                # Step from lowest to highest iqy
+                for t_iqy in range(iqy_params[0], iqy_params[1], iqy_step):
+                    yiq_iqy = t_iqy / 100
+                    self.add_rgb(tuple([yiq_y, yiq_iqx, yiq_iqy]), color_space='yiq')
+
+        return list(self.colors.keys())
+
+    def color_gen_list_from_hsv(self, num_cols, debug=False):
 
         # Calculate optimal number of steps for num_cols distinct colors
         num_hues = 14
@@ -89,10 +139,10 @@ class ColorUgen:
                     if hsv_val > 0.90: m_hsv_val = hsv_val * 0.85 if 85 < t_hue < 140 else hsv_val
                     else: m_hsv_val = hsv_val
                     hsv_hue = float(t_hue) / 360.0
-                    self.add_hsv(tuple([hsv_hue, hsv_sat, m_hsv_val]))
+                    self.add_rgb(tuple([hsv_hue, hsv_sat, m_hsv_val]), color_space='hsv')
                 start_hue += int(h_step / total_sv_steps)
                 if debug: print('start_hue: {}'.format(start_hue))
-        return list(self.colors.keys())[:num_cols]
+        return list(self.colors.keys())
 
     def print(self):
         print('Added hsv: {}, Resultant rgb: {}'.format(self.counter, len(self.colors)))
